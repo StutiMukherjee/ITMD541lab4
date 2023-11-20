70f4c1771efee4ef2d619efb0f1d7c5280c6ac51
StutiMukherjee <144186144+StutiMukherjee@users.noreply.github.com>
2023-11-19 21:22:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Function to fetch coordinates from the geocode API
+// script.js
 async function getCoordinates(location) {
     const apiKey = '2a75792aaed5c771767ef5ee42f94c3d';
-    const geocodeUrl = `https://geocode.maps.co/?location=${location}&key=${apiKey}`;
+    const geocodeUrl = `https://geocode.maps.com/?location=${location}&key=${apiKey}`;
 
     try {
         const response = await fetch(geocodeUrl);
@@ -15,45 +15,51 @@ async function getCoordinates(location) {
         }
     } catch (error) {
         console.error('Error fetching coordinates:', error);
-        return null; // Return null to signify an error
+        return null;
     }
 }
 
-// Function to fetch sunrise and sunset using coordinates from Sunrise-Sunset.org API
 async function getSunriseSunset(lat, lng) {
     const sunriseSunsetUrl = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&formatted=0`;
-    
+
     try {
         const response = await fetch(sunriseSunsetUrl);
         const data = await response.json();
-        
-        // Parse the response and extract sunrise and sunset times
+
         const { sunrise, sunset } = data.results;
         return { sunrise, sunset };
     } catch (error) {
         console.error('Error fetching sunrise/sunset:', error);
-        return null; // Return null to signify an error
+        return null;
     }
 }
 
-// Example usage
 async function fetchSunriseSunsetInfo() {
-    const userLocation = prompt("Enter a named location:");
-    
+    const userLocation = document.getElementById('searchInput').value;
+
     const coordinates = await getCoordinates(userLocation);
     if (coordinates) {
         const { lat, lng } = coordinates;
         const sunriseSunset = await getSunriseSunset(lat, lng);
-        
+
         if (sunriseSunset) {
-            console.log(`Sunrise: ${sunriseSunset.sunrise}\nSunset: ${sunriseSunset.sunset}`);
+            displaySunriseSunset(userLocation, sunriseSunset);
         } else {
-            console.log('Error fetching sunrise/sunset information.');
+            displayError();
         }
     } else {
-        console.log('Location not found or an error occurred.');
+        displayError();
     }
 }
 
-// Call the function to initiate the process
-fetchSunriseSunsetInfo();
+function displaySunriseSunset(location, sunriseSunset) {
+    document.getElementById('locationName').textContent = location;
+    document.getElementById('sunriseTime').textContent = sunriseSunset.sunrise;
+    document.getElementById('sunsetTime').textContent = sunriseSunset.sunset;
+}
+
+function displayError() {
+    document.getElementById('locationName').textContent = 'Location not found';
+    document.getElementById('sunriseTime').textContent = 'N/A';
+    document.getElementById('sunsetTime').textContent = 'N/A';
+}
