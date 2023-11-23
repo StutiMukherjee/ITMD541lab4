ae19bd89206b63ca48834044914351989ecd4951
StutiMukherjee <144186144+StutiMukherjee@users.noreply.github.com>
2023-11-22 22:08:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-/ script.js
+//script.js
 async function getCoordinates(location) {
-  
+
     const geocodeUrl = `https://geocode.maps.co/search?q=${location}`;
-    
+
     try {
         let response = await fetch(geocodeUrl);
         let data = await response.json();
@@ -14,20 +14,23 @@ async function getCoordinates(location) {
     }
 }
 
-async function getSunriseSunsettoday(lat, lng) {
-    const sunriseSunsetUrl = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`;
+async function getSunriseSunset(lat, lng, date) {
+    const sunriseSunsetUrl = 'https://api.sunrisesunset.io/json?lat=' + lat + '&lng=' + lng + '&date=' + date;
 
     try {
         const response = await fetch(sunriseSunsetUrl);
         const data = await response.json();
 
-        const { sunrise, sunset } = data.results;
-        return { sunrise, sunset };
+        const { sunrise, sunset, dawn, dusk, day_length, solar_noon, timezone } = data.results;
+        return { sunrise, sunset, dawn, dusk, day_length, solar_noon, timezone };
     } catch (error) {
         console.error('Error fetching sunrise/sunset:', error);
         return null;
     }
 }
+
+
+
 
 async function fetchSunriseSunsetInfo() {
     const userLocation = document.getElementById('searchInput').value;
@@ -35,10 +38,13 @@ async function fetchSunriseSunsetInfo() {
     const coordinates = await getCoordinates(userLocation);
     if (coordinates) {
         const { lat, lng } = coordinates;
-        const sunriseSunset = await getSunriseSunsettoday(lat, lng);
+        const sunriseSunsetToday = await getSunriseSunset(lat, lng, 'today');
+        const sunriseSunsetTomorrow = await getSunriseSunset(lat, lng, 'tomorrow');
 
-        if (sunriseSunset) {
-            displaySunriseSunset(userLocation, sunriseSunset);
+
+       if (sunriseSunsetToday) {
+            displaySunriseSunsetToday(userLocation, sunriseSunsetToday);
+            displaySunriseSunsetTomorrow(userLocation, sunriseSunsetTomorrow);
         } else {
             displayError();
         }
@@ -47,10 +53,29 @@ async function fetchSunriseSunsetInfo() {
     }
 }
 
-function displaySunriseSunset(location, sunriseSunset) {
-    document.getElementById('locationName').textContent = location;
-    document.getElementById('sunriseTime').textContent = sunriseSunset.sunrise;
-    document.getElementById('sunsetTime').textContent = sunriseSunset.sunset;
+function displaySunriseSunsetToday(location, sunriseSunset) {
+    document.getElementById('locationNameToday').textContent = location;
+    document.getElementById('sunriseTimeToday').textContent = sunriseSunset.sunrise;
+    document.getElementById('sunsetTimeToday').textContent = sunriseSunset.sunset;
+    document.getElementById('dawnTimeToday').textContent = sunriseSunset.dawn;
+    document.getElementById('duskTimeToday').textContent = sunriseSunset.dusk;
+    document.getElementById('dayLengthToday').textContent = sunriseSunset.day_length;
+    document.getElementById('solarNoonTimeToday').textContent = sunriseSunset.solar_noon;
+    document.getElementById('timezoneToday').textContent = sunriseSunset.timezone;
+
+}
+
+function displaySunriseSunsetTomorrow(location, sunriseSunset) {
+    ///// create new html code for tomorrow and change fields here
+    document.getElementById('locationNameTomorrow').textContent = location;
+    document.getElementById('sunriseTimeTomorrow').textContent = sunriseSunset.sunrise;
+    document.getElementById('sunsetTimeTomorrow').textContent = sunriseSunset.sunset;
+    document.getElementById('dawnTimeTomorrow').textContent = sunriseSunset.dawn;
+    document.getElementById('duskTimeTomorrow').textContent = sunriseSunset.dusk;
+    document.getElementById('dayLengthTomorrow').textContent = sunriseSunset.day_length;
+    document.getElementById('solarNoonTimeTomorrow').textContent = sunriseSunset.solar_noon;
+    document.getElementById('timezoneTomorrow').textContent = sunriseSunset.timezone;
+
 }
 
 function displayError() {
